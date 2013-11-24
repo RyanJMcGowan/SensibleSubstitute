d32224b96b4b2045a8d86a389d63f2efe18db834
Ryan <civicdesigngroup@yahoo.com>
2013-11-24 14:56:35 -0800

    Added Store Controller

---
+++ SensibleSubstitute/Controllers/StoreController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SensibleSubstitute.Controllers
+{
+    public class StoreController : Controller
+    {
+        //This controller is instantiated by the URL "~/partials/store"
+        public ActionResult Store()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ SensibleSubstitute/src/app/store/store.spec.js
@@ -0,0 +1,9 @@
+﻿describe('store section', function ()
+{
+	beforeEach(module('SensibleApp'));
+
+	it('should have a dummy test', inject(function ()
+	{
+		expect(true).toBeTruthy();
+	}));
+});
